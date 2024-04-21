6e2da9099b53a14da43aed9322c01c0b8f425546
Steve <steve75918@gmail.com>
2024-04-22 05:02:29 +0800

    [Optimize] Remove all output but response of API.

--- app/Http/Controllers/TT2Controller.php
+++ app/Http/Controllers/TT2Controller.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\TT2Request;
 
 class TT2Controller extends Controller
 {
-    public function raidTargetCalc(Request $request)
+    public function raidTargetCalc(TT2Request $request)
     {
-        $params = $request->post('inputs');
-
+        // process data
+        $params = $request->validated('inputs');
         $parts = [];
 
         // Head
@@ -44,6 +44,7 @@ class TT2Controller extends Controller
         ];
 
         for ($i = 0; $i < 4; $i ++) {
+            $arm['name'] = sprintf('%s-%d', 'Arm', $i + 1);
             array_push($parts, $arm);
         }
 
@@ -59,6 +60,7 @@ class TT2Controller extends Controller
         ];
 
         for ($i = 0; $i < 2; $i ++) {
+            $leg['name'] = sprintf('%s-%d', 'Leg', $i + 1);
             array_push($parts, $leg);
         }
 
@@ -66,10 +68,7 @@ class TT2Controller extends Controller
         $collection = collect($parts);
         $targetValue = $params[8];
 
-        // $msg = sprintf("<div>Calculating...</div>");
-        // echo $msg;
-
-        // Calculate selected targets until exceed target
+        // Select targets by ratio
         $sortedTargets = $collection->sortBy('ratio');
 
         $totalHp = 0.0;
@@ -86,39 +85,15 @@ class TT2Controller extends Controller
 
         $selectedTargets = $sortedTargets->chunk($count)->first();
 
-        // $msg = sprintf("<div>Done! Got %d targets.</div>", $selectedTargets->count());
-        // echo $msg;
-
         // Exclude certain result from exceed more than a target part
         $exceedValue = $totalHp - $targetValue;
 
         foreach ($selectedTargets->reverse() as $key => $target) {
             if ($exceedValue >= $target['hp']) {
-                // $removed = $selectedTargets->get($key);
-                // $msg = sprintf('<div>Removing: %s, with %.4f hp</div>', $removed['name'], $removed['hp']);
-                // echo $msg;
-
                 $selectedTargets->pull($key);
                 $exceedValue -= $target['hp'];
             }
         }
-
-        // echo '<hr />';
-        // foreach ($selectedTargets as $finalTarget) {
-        //     printf("<div>Name: %s</div>", $finalTarget['name']);
-        //     printf("<div>Hp: %.4f</div>", $finalTarget['hp']);
-        //     printf("<div>Ap: %.4f</div>", $finalTarget['ap']);
-        //     printf("<div>Ratio: %.4f</div>", $finalTarget['ratio']);
-        //     echo '<br />';
-        // }
-
-        // $actulValue = $selectedTargets->sum('hp');
-        // printf(
-        //     'Target: %.4f, Total: %.4f, Exceed: %.4f',
-        //     $targetValue,
-        //     $actulValue,
-        //     $actulValue - $targetValue
-        // );
 
         return response()->json($selectedTargets);
     }
--- tests/Feature/Http/Controllers/TT2ControllerTest.php
+++ tests/Feature/Http/Controllers/TT2ControllerTest.php
@@ -17,7 +17,7 @@ class TT2ControllerTest extends TestCase
             'inputs' => [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0],
         ];
 
-        $response = $this->post('/api/tt2/raid_target', $inputParams);
+        $response = $this->postJson('/api/tt2/raid_target', $inputParams);
 
         $response->assertOk()
             ->assertJsonStructure([
@@ -28,5 +28,90 @@ class TT2ControllerTest extends TestCase
                     'ratio',
                 ]
             ]);
+    }
+
+    /**
+     * Don't have required parameters
+     *
+     * @return void
+     */
+    public function test_missing_required_parameters()
+    {
+        $inputParams = [
+            'inputs' => [],
+        ];
+
+        $response = $this->postJson('/api/tt2/raid_target', $inputParams);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'inputs'
+            ]);
+    }
+
+    /**
+     * Input type error
+     *
+     * @return void
+     */
+    public function test_invalid_parameter_type()
+    {
+        $inputParams = [
+            'inputs' => ['abc', 'def', 'ghi', 'jkl', 'mno', 'pqr', 'stu', 'vwx', 'yz'],
+        ];
+
+        $response = $this->postJson('/api/tt2/raid_target', $inputParams);
+
+        $response->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'inputs.0',
+                'inputs.1',
+                'inputs.2',
+                'inputs.3',
+                'inputs.4',
+                'inputs.5',
+                'inputs.6',
+                'inputs.7',
+                'inputs.8',
+            ])
+            ->assertJsonFragment([
+                'inputs.0' => ['The inputs.0 field must be a number.'],
+                'inputs.1' => ['The inputs.1 field must be a number.'],
+                'inputs.2' => ['The inputs.2 field must be a number.'],
+                'inputs.3' => ['The inputs.3 field must be a number.'],
+                'inputs.4' => ['The inputs.4 field must be a number.'],
+                'inputs.5' => ['The inputs.5 field must be a number.'],
+                'inputs.6' => ['The inputs.6 field must be a number.'],
+                'inputs.7' => ['The inputs.7 field must be a number.'],
+                'inputs.8' => ['The inputs.8 field must be a number.'],
+            ]);
+    }
+
+    /**
+     * Input out of range
+     *
+     * @return void
+     */
+    public function test_invalid_parameter_range()
+    {
+        $inputParams = [
+            'inputs' => [1000.0, -10.0, 999.9999999, 0.00000001, 1.0, 2.0, 3.2, 4.0, 120.90],
+        ];
+
+        $response = $this->postJson('/api/tt2/raid_target', $inputParams);
+
+        $response->assertStatus(422)
+        ->assertJsonValidationErrors([
+            'inputs.0',
+            'inputs.1',
+            'inputs.2',
+            'inputs.3',
+        ])
+        ->assertJsonFragment([
+            'inputs.0' => ['The inputs.0 field must be between 0.1 and 999.9.'],
+            'inputs.1' => ['The inputs.1 field must be between 0.1 and 999.9.'],
+            'inputs.2' => ['The inputs.2 field must be between 0.1 and 999.9.'],
+            'inputs.3' => ['The inputs.3 field must be between 0.1 and 999.9.'],
+        ]);
     }
 }
